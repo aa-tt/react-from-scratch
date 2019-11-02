c59b19d3871ac7c6838f9ab0309a31ac77eb7c71
Anunay Anindya <>
2019-11-02 20:09:53 +0530

    added ToDo react app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import ToDo from './ToDo';
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ToDo />, document.getElementById("root-Todo"));
